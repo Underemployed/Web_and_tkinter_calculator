300abe655b017be25d376f1d7e278ce8c0481573
Underemployed <underemployed871@gmail.com>
2022-11-02 19:46:38 +0530

    Colorful text generator 2

--- button-2.py
+++ button-2.py
@@ -7,16 +7,17 @@ root = Tk()
 e=Entry(root,width=50)
 e.pack()
 
-def function():
+def randomcolorgenerator():
     a=['red','green','blue','yellow','black','white','orange','pink']
     b=random.randint(0,7)
     return a[b]
 
 def myClick():
     
-    myLabel=Label(root, text="Click me again!",fg=function())
+    myLabel=Label(root, text="Click me again!",fg=randomcolorgenerator())
     myLabel.pack()
     
+
 myButton =Button(root,text="Click Me!",padx =10,pady=3,command=myClick,fg="white",bg="blue")
 myButton.pack()
 
