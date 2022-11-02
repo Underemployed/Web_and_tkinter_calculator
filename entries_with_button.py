3b74d821255efb3670c61b0d428d0464d2ba2415
Underemployed <underemployed871@gmail.com>
2022-11-02 20:41:55 +0530

    now generated Hello+name in random color

--- entries_with_button.py
+++ entries_with_button.py
@@ -6,11 +6,20 @@ root=Tk()
 e= Entry(root, width=30)
 e.pack()
 
+def randomColor():
+    list="grey","orange","white", "black", "red", "green", "blue", "cyan", "yellow",  "magenta","indigo","voilet"
+    b=random.randint(0,len(list)-1)
+    d=list[b],list[b-1],list[b-2]
+    return d[random.randint(0,2)]
+    
 
 def onClick():
-    a=e.get()
+    b=randomColor()
+    c=randomColor()
+    a="Hello "+str(e.get())
     e.delete(0,END)
-    e.insert(0,"Hello "+a)
+    myLabel=Label(root, text=a, fg=str(b), bg=str(c))
+    myLabel.pack()
 
 myButton=Button(root, text="Enter Your Name:", command=onClick)
 
