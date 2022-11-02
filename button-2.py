22a13e189891c7393e1261fc5557f644dae2e4bb
dumbfuckjuic <nithinasokancoc@gmail.com>
2022-11-02 19:36:50 +0530

    colurfulu=output really fun button.py

--- button-2.py
+++ button-2.py
@@ -1,14 +1,24 @@
 from tkinter import *
-from random import *
+import random
 from click import command 
 
 root = Tk()
 
+e=Entry(root,width=50)
+e.pack()
+
+def function():
+    a=['red','green','blue','yellow','black','white','orange','pink']
+    b=random.randint(0,8)
+    return a[b]
+
 def myClick():
     
-    myLabel=Label(root, text="Click me again!",fg="red")
+    myLabel=Label(root, text="Click me again!",fg=function())
     myLabel.pack()
+    
 myButton =Button(root,text="Click Me!",padx =10,pady=3,command=myClick,fg="white",bg="blue")
 myButton.pack()
+
 
 root.mainloop()
