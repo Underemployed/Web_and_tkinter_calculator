7be53be497eee7cfc15193d1259a952742a7b80f
Underemployed <underemployed871@gmail.com>
2023-04-16 05:57:29 +0630

    simple calculator with basic features

---
+++ script.js
@@ -0,0 +1,88 @@
+const display = document.getElementById("display");
+
+display.addEventListener("input", function() {
+  // 
+  const value = display.value;
+  console.log(value);
+});
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function removeLastCharacter() {
+  const value = display.value;
+  display.value = value.substring(0, value.length - 1);
+}
+
+function addToDisplay(character) {
+  display.value += character;
+}
+
+function calculateResult() {
+  const expression = display.value;
+  if (checkInput(expression)) {
+    const result = calculate(expression);
+    display.value = result;
+  }
+}
+
+function checkInput(expression) {
+
+  const regex = /^[\d\s\(\)\+\-\*\/\.]+$/;
+  if (!regex.test(expression)) {
+    
+    display.value = "Invalid input!";
+    
+    return false;
+  }
+  return true;
+}
+
+
+function calculate(s) {
+function update(op, v) {
+if (op == "+") stack.push(v);
+if (op == "-") stack.push(-v);
+if (op == "*") stack.push(Math.floor(stack.pop() * v));
+if (op == "/") stack.push(Math.floor(stack.pop() / v));
+}
+
+var stack = [];
+var num = 0;
+var op = "+";
+for (var i = 0; i < s.length; i++) {
+var c = s.charAt(i);
+if (c >= "0" && c <= "9") {
+num = num * 10 + parseInt(c);
+} else if (c == "(") {
+stack.push(op);
+op = "+";
+} else if (c == ")") {
+update(stack.pop(), num);
+op = "+";
+} else if (c == "+" ||  c == "-" || c == "*" || c == "/") {
+update(op, num);
+num = 0;
+op = c;
+}
+}
+update(op, num);
+
+var result = 0;
+for (var i = 0; i < stack.length; i++) {
+result += stack[i];
+}
+return result;
+
+}
+document.getElementById("display").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+      calculateResult();
+  }});
+
+display.addEventListener("input", function() {
+
+const value = display.value;
+console.log(value);
+});
