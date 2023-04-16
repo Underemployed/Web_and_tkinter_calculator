bb6a1e5749c07dc15d03a9df9fe8b4be0b1e04e8
Underemployed <underemployed871@gmail.com>
2023-04-16 07:05:40 +0630

    improved the backend logic for calculation

--- script.js
+++ script.js
@@ -1,115 +1,152 @@
-const display = document.getElementById("display");
-
-display.addEventListener("input", function() {
-  // 
-  const value = display.value;
-  console.log(value);
-});
-
-function clearDisplay() {
-  display.value = "";
-}
-
-function removeLastCharacter() {
-  const value = display.value;
-  display.value = value.substring(0, value.length - 1);
-}
-
-function addToDisplay(character) {
-  display.value += character;
-}
-
-function calculateResult() {
-  const expression = display.value;
-  if (checkInput(expression)) {
-    const result = calculate(expression);
-    display.value = result;
-  }
-}
-
-function checkInput(expression) {
-
-  const regex = /^[\d\s\(\)\+\-\*\/\.]+$/;
-  if (!regex.test(expression)) {
-    
-    display.value = "Invalid input!";
-    
-    return false;
-  }
-  return true;
-}
-
-
-function calculate(s) {
-function update(op, v) {
-if (op == "+") stack.push(v);
-if (op == "-") stack.push(-v);
-if (op == "*") stack.push(Math.floor(stack.pop() * v));
-if (op == "/") stack.push(Math.floor(stack.pop() / v));
-}
-
-var stack = [];
-var num = 0;
-var op = "+";
-for (var i = 0; i < s.length; i++) {
-  var c = s.charAt(i);
-
-  if (c >= "0" && c <= "9") {
-    num = num * 10 + parseInt(c);
-} else if (c == "(") {
-stack.push(op);
-op = "+";
-} else if (c == ")") {
-update(stack.pop(), num);
-op = "+";
-} else if (c == "+" ||  c == "-" || c == "*" || c == "/") {
-update(op, num);
-num = 0;
-op = c;
-}
-}
-update(op, num);
-
-var result = 0;
-for (var i = 0; i < stack.length; i++) {
-result += stack[i];
-}
-return result;
-
-} 
-
-display.addEventListener("input", function() {
-
-const value = display.value;
-console.log(value);
-});
-
-document.getElementById("display").addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-      calculateResult();
-  }});
-
-display.addEventListener("input", function() {
-
-const value = display.value;
-console.log(value);
-
-
-});
 function toggleMode() {
   const body = document.querySelector("body");
   const switchBtn = document.querySelector(".switch input[type='checkbox']");
 
   if (switchBtn.checked) {
     body.classList.add("dark-mode");
-    calculator.classList.add("dark-mode");
     body.classList.remove("light-mode");
-    calculator.classList.add("dark-mode");
-    calculator.classList.remove("dark-mode");
   } else {
     body.classList.add("light-mode");
     body.classList.remove("dark-mode");
-    calculator.classList.add("light-mode");
-    calculator.classList.remove("dark-mode");
   }
 }
+
+    const display = document.getElementById("display");
+
+    
+
+      function clearDisplay() {
+        display.value = "";
+      }
+
+      function removeLastCharacter() {
+        const value = display.value;
+        display.value = value.substring(0, value.length - 1);
+      }
+
+      function addToDisplay(character) {
+        display.value += character;
+      }
+
+      function calculateResult() {
+        const expression = display.value;
+        if (checkInput(expression)) {
+          const result = calculate(expression);
+          display.value = result;
+        }
+      }
+
+      function checkInput(expression) {
+        // Check if the input contains only numbers, operators, and parentheses
+        const regex = /^[\d\s\(\)\+\-\*\/\.]+$/;
+        if (!regex.test(expression)) {
+          // Display an error message in the display area
+          display.value = "Invalid input!";
+          // Reset the input to the previous value
+          return false;
+        }
+        return true;
+      }
+
+
+// uses getprecedence and applyoperation function for calculation
+function calculate(expression) {
+    //removes emplty spaces
+  expression = expression.replace(/\s+/g, '');
+      //use arrays to keep track operators and operands
+
+  var operators = [];
+  var operands = [];
+  for (var i = 0; i < expression.length; i++) {
+    var char = expression.charAt(i);
+    if (/\d/.test(char) || char === '.') {
+      var start = i;
+      while (i < expression.length && (/[\d.]/.test(expression.charAt(i)))) {
+        i++;
+      }
+      operands.push(parseFloat(expression.substring(start, i)));
+      i--;
+    } else if (/[+\-*/^()%]/.test(char)) {
+      if (char === '(') {
+        operators.push(char);
+      } else if (char === ')') {
+        while (operators[operators.length - 1] !== '(') {
+          var operator = operators.pop();
+          var operand2 = operands.pop();
+          var operand1 = operands.pop();
+          operands.push(applyOperation(operator, operand1, operand2));
+        }
+        operators.pop();
+      } else {
+        while (operators.length > 0 && getPrecedence(operators[operators.length - 1]) >= getPrecedence(char)) {
+          var operator = operators.pop();
+          var operand2 = operands.pop();
+          var operand1 = operands.pop();
+          operands.push(applyOperation(operator, operand1, operand2));
+        }
+        operators.push(char);
+      }
+    } else {
+      throw new Error('Invalid character: ' + char);
+    }
+  }
+  while (operators.length > 0) {
+    var operator = operators.pop();
+    var operand2 = operands.pop();
+    var operand1 = operands.pop();
+    operands.push(applyOperation(operator, operand1, operand2));
+  }
+  return operands.pop();
+}
+
+function getPrecedence(operator) {
+    //for managing priority of the operator decides which should run first
+  switch (operator) {
+    case '+':
+    case '-':
+      return 1;
+    case '*':
+    case '/':
+      return 2;
+    case '^':
+      return 3;
+    case '%':
+      return 4;
+    default:
+      return 0;
+  }
+}
+
+function applyOperation(operator, operand1, operand2) {
+    //takes operator and two numbers as input and applies the operator
+    switch (operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '*':
+      return operand1 * operand2;
+    case '/':
+      return operand1 / operand2;
+    case '%':
+      return operand1 % operand2;
+    case '^':
+      return Math.pow(operand1, operand2);
+    default:
+      throw new Error('Invalid operator: ' + operator);
+  }
+}
+
+
+document.getElementById("display").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+        //   EXECUTES THE EXPRESSION ON PRESSING ENTER ON KEYBOARD
+            calculateResult();
+        }});
+
+display.addEventListener("input", function() {
+    // TAKES BOTH TYPED AND INPUT THRU BUTTONS USING EVENT LISTENER AND DISPLAYS  INSIDE INPUT
+    
+    const value = display.value;
+    console.log(value);
+});
